27ca482cf523fe565d9c14897c943bee419190ff
Diego Alejandro Hidalgo Machaca <135664425+DiegoH81@users.noreply.github.com>
2025-10-08 21:21:13 -0500

    Update GTR-04_Validar.php

--- gtr/GTR-04_Validar.php
+++ gtr/GTR-04_Validar.php
@@ -4,21 +4,22 @@ require_once 'GTR-01_GestionarUsuario.php';
 
 class Validar {
 
-    // FUN- validarUsuario
+    // FUN-16 validarUsuario
     public static function validarUsuario($usuario) {
         return GestionarUsuario::validarUsuario($usuario);
     }
 
-    // FUN- validarCredenciales
+    // FUN-17 validarCredenciales
     public static function validarCredenciales($usuario, $contrasena) {
         return GestionarUsuario::validarCredenciales($usuario, $contrasena);
     }
 
-    // FUN- usuarioDisponible
+    // FUN-18 usuarioDisponible
     public static function usuarioDisponible($usuario) {
         return GestionarUsuario::usuarioDisponible($usuario);
     }
 
+    // FUN-19 obtenerUsuario
     public static function obtenerUsuario($usuario) {
         return GestionarUsuario::obtenerUsuario($usuario);
     }
