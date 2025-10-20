68aa0e7bc51d880ef0fa238523909e7ddc9b524a
Diego Alejandro Hidalgo Machaca <135664425+DiegoH81@users.noreply.github.com>
2025-10-20 11:10:32 -0500

    Update GTR-04_Validar.php

--- gtr/GTR-04_Validar.php
+++ gtr/GTR-04_Validar.php
@@ -6,30 +6,30 @@ require_once 'GTR-01_GestionarUsuario.php';
 
 class Validar {
 
-    /* FUN-15 validarUsuario 
+    /* FUN-15 solicitarValidacionUsuario
         Invoca al GTR-01 Gestionar usuario para verificar si el usaurio ingresado existe */
-    public static function validarUsuario($usuario) {
+    public static function solicitarValidacionUsuario($usuario) {
         return GestionarUsuario::validarUsuario($usuario);
     }
 
-    /* FUN-16 validarCredenciales 
+    /* FUN-16 solicitarValidacionCredenciales 
         Invoca al GTR-01 Gestionar usuario para verificar si la contraseña ingresada coincide
         con la del usuario ingresado */
-    public static function validarCredenciales($usuario, $contrasena) {
+    public static function solicitarValidacionCredenciales($usuario, $contrasena) {
         return GestionarUsuario::validarCredenciales($usuario, $contrasena);
     }
 
-    /* FUN-17 usuarioDisponible 
+    /* FUN-17 solicitarUsuarioDisponible 
         Invoca al GTR-01 Gestionar usuario para verificar si el usuario(nombre de usuario)
         esta en uso */
-    public static function usuarioDisponible($usuario) {
+    public static function solicitarUsuarioDisponible($usuario) {
         return GestionarUsuario::usuarioDisponible($usuario);
     }
 
-    /* FUN-18 obtenerUsuario 
+    /* FUN-18 solicitarUsuario 
         Invoca al GTR-01 Gestionar usuario para extraer los datos de un usuario especifico 
         segun su id */
-    public static function obtenerUsuario($usuario) {
+    public static function solicitarUsuario($usuario) {
         return GestionarUsuario::obtenerUsuario($usuario);
     }
 }
